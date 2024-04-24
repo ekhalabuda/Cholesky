810f8a77e86cfb14a762e28097ae8ade3ecf70be
ekhalabuda <112930468+ekhalabuda@users.noreply.github.com>
2024-04-25 01:15:35 +0300

    25.04.24 compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-g++ -Wall -O3 -std=c++11 Cholesky.cpp -o Cholesky
+#g++ -Wall -O3 -fopenmp -std=c++11 Cholesky.cpp -o Cholesky
+g++ -Wall -O3 -fopenmp -std=c++11 -I/opt/intel/oneapi/mkl/2024.1/include Cholesky.cpp -L/opt/intel/oneapi/mkl/2024.1/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm -o Cholesky
